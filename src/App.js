d0934757c38b6f89268a3860fb7834d3e08b3157
Saksham Verma <sakshamverma1000@gmail.com>
2024-07-07 18:43:15 +0530

    Adding TextUtils Project

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+// import About from "./components/About";
+import Form from "./components/Form";
+import Alert from "./components/Alert";
+import { useState } from "react";
+// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [modeText, setModeText] = useState("Dark");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      setModeText("Light");
+      document.body.style.backgroundColor = "rgb(16, 15, 32)";
+      showAlert("Dark Mode has been Enabled", "success");
+    } else {
+      setMode("light");
+      setModeText("Dark");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been Enabled", "success");
+    }
+  };
+
+  const enableGreenMode = () => {
+    setMode("success");
+    document.body.style.backgroundColor = "rgb(5, 33, 5)";
+    showAlert("Green Mode has been Enabled", "success");
+    // HAVE TO CHANGE TEXT TO wHITE
+  };
+
+  const enableMaroonMode = () => {
+    setMode("danger");
+    document.body.style.backgroundColor = "maroon";
+    showAlert("Maroon Mode has been Enabled", "success");
+  };
+
+  const enableGreyMode = () => {
+    setMode("secondary");
+    document.body.style.backgroundColor = "grey";
+    showAlert("Grey Mode has been Enabled", "success");
+  };
+
+  return (
+    <>
+      {/*<Router>*/}
+      <NavBar
+        title="Text Utils"
+        modeText={modeText}
+        aboutText="About"
+        mode={mode}
+        toggleMode={toggleMode}
+        green={enableGreenMode}
+        maroon={enableMaroonMode}
+        grey={enableGreyMode}
+      />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Routes>
+            <Route
+              exact
+              path="/"
+              element={ */}
+        <Form heading="Enter the Text" mode={mode} showAlert={showAlert} />
+        {/* }
+            />
+
+            <Route path="/about" element={<About />} />
+          </Routes> */}
+
+        {/* </Router> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,159 @@
+import React, { useState } from "react";
+
+export default function Form(props) {
+  const [text, setText] = useState(" ");
+
+ 
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleUpperCase = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to UpperCase", "success");
+  };
+
+  const handleLowerCase = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to LowerCase", "success");
+  };
+
+  const Clearwindow = () => {
+    setText("");
+    props.showAlert("Terminal has been cleared !", "success");
+  };
+
+  const handleInverse = () => {
+    let newText = "";
+
+    for (let f = 0; f < text.length; f++) {
+      let character = text.charAt(f);
+
+      if (character === character.toUpperCase()) {
+        newText += character.toLowerCase();
+      } else if (character === character.toLowerCase()) {
+        newText += character.toUpperCase();
+      } else {
+        newText += character;
+      }
+    }
+
+    setText(newText);
+    props.showAlert("converted to InVeRsE CaSe !", "success");
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    props.showAlert("Copied to Clipboard !", "success");
+  };
+  // *************************************
+
+  // GETTING word count and character count
+  let wordCount = 0;
+  let charCount = 0;
+  let charArr = text.split("");
+  let wordArr = text.split(" ");
+
+  const get_word_count = (count, arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== " " && arr[i] !== "") {
+        count++;
+      }
+    }
+
+    return count;
+  };
+
+  const getCharCount = (charCount, arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== "" && arr[i] !== " ") {
+        charCount++;
+      }
+    }
+
+    return charCount;
+  };
+
+  // ************************************************
+
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>{props.heading}</h1>
+
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+            onChange={handleOnChange}
+            value={text}
+            id="exampleFormControlTextarea1"
+            rows="10"
+          ></textarea>
+        </div>
+
+        <button
+          id="Button1"
+          className={`btn btn-primary mx-3`}
+          onClick={handleUpperCase}
+        >
+          Convert To UpperCase
+        </button>
+        <button
+          id="Button2"
+          className="btn btn-primary mx-3"
+          onClick={handleLowerCase}
+        >
+          Convert To LowerCase
+        </button>
+
+        <button
+          id="Button3"
+          className="btn btn-primary mx-3"
+          onClick={Clearwindow}
+        >
+          Clear
+        </button>
+
+        <button
+          id="Button4"
+          className="btn btn-primary mx-3"
+          onClick={handleInverse}
+        >
+          InVeRsE CaSe
+        </button>
+
+        <button
+          id="Button5"
+          className="btn btn-primary mx-3"
+          onClick={handleCopy}
+        >
+          Copy to Clipboard
+        </button>
+      </div>
+
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>Your Text Summary:</h1>
+        <p>
+          words: {get_word_count(wordCount, wordArr)}
+          <br />
+          characters: {getCharCount(charCount, charArr)}
+        </p>
+
+        <h2>Preview</h2>
+        <p>{text.length <= 1 ? "Enter something to preview Here" : text}</p>
+      </div>
+    </>
+  );
+}
